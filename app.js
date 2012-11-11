52188114dd1ff2b58410d6dac29ad48c9da4b57d
Pascal Garber <jumplink@gmail.com>
2012-11-11 22:28:52 +0100

    messerkategorie mit neuen buttons

--- app.js
+++ app.js
@@ -3,20 +3,21 @@ var express = require('express');
 var port = parseInt(process.argv.pop());
 var app = express.createServer();
 app.configure(function() {
+	app.use(express.static(__dirname + '/public'));
     app.use(express.bodyParser());
     app.use(app.router);
 });
 
 app.set('view engine', 'jshtml');
 app.get('/', function(req, res) { res.render('startseite', {}); });
-app.get('messer', function(req, res) { res.render('messer', {}); });
-app.get('musik', function(req, res) { res.render('musik', {}); });
-app.get('schloesser', function(req, res) { res.render('schloesser', {}); });
-app.get('stifte', function(req, res) { res.render('stifte', {}); });
-app.get('taschenlampen', function(req, res) { res.render('taschenlampen', {}); });
-app.get('tiere', function(req, res) { res.render('tiere', {}); });
-app.get('unterwegs', function(req, res) { res.render('unterwegs', {}); });
-app.get('visitenkarten', function(req, res) { res.render('visitenkarten', {}); });
-app.get('zippos', function(req, res) { res.render('zippos', {}); });
+app.get('/messer', function(req, res) { res.render('messer', {}); });
+app.get('/musik', function(req, res) { res.render('musik', {}); });
+app.get('/schloesser', function(req, res) { res.render('schloesser', {}); });
+app.get('/stifte', function(req, res) { res.render('stifte', {}); });
+app.get('/taschenlampen', function(req, res) { res.render('taschenlampen', {}); });
+app.get('/tiere', function(req, res) { res.render('tiere', {}); });
+app.get('/unterwegs', function(req, res) { res.render('unterwegs', {}); });
+app.get('/visitenkarten', function(req, res) { res.render('visitenkarten', {}); });
+app.get('/zippos', function(req, res) { res.render('zippos', {}); });
 
 app.listen(port);
